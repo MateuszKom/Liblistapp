f8fb3fd11c28201a4f0948127300e0226ff254b5
komperda.mat@gmail.com <komperda.mat@gmail.com>
2022-03-31 14:30:29 +0200

    Add project files.

---
+++ Repositories/IGameRepository.cs
@@ -0,0 +1,17 @@
+﻿using Liblistapp.Models;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Liblistapp.Repositories
+{
+    public interface IGameRepository
+    {
+        Task<IEnumerable<Game>> Get();
+        Task<Game> Get(int id);
+        Task<Game> Create(Game game);
+        Task Update(Game game);
+        Task Delete(int id);
+    }
+}
